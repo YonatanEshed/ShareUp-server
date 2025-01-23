87d121c426714d987d134cd169fe47c57e0c6969
Yonatan Eshed <yonatan.eshed@gmail.com>
2025-01-23 12:12:01 +0200

    feat: add profile feature - get and update profile

---
+++ src/features/users/routes/profile.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+    getOwnProfile,
+    getProfile,
+    updateProfile,
+} from '../controllers/profile.controller';
+
+const router = Router();
+
+// based on req.user
+router.put('/', updateProfile); // update profile
+router.put('/profilePicture'); // update profile picture
+router.get('/', getOwnProfile); // get own user profile
+
+router.get('/:userId', getProfile); // get user's profile
+
+router.post('/:userId/follow'); // follow userId
+router.delete('/:userId/follow'); // unfollow userId
+router.get('/:userId/followers'); // get list of userId's followers
+router.get('/:userId/followings'); // get list of userId's followers
+
+export default router;
