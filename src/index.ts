1950b8c9666ea742040918a59623ea97ab93166e
Yonatan Eshed <yonatan.eshed@gmail.com>
2025-04-25 15:39:06 +0300

    fix: uncomment Firebase functions import and export API handler

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import app from './app';
 // import config from '@/config/env';
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
 const main = express();
 main.use('/api', app);
 
-const port = 3000;
-main.listen(port, () => {
-    console.log(`server is running at http://localhost:${port}`);
-});
+// const port = 3000;
+// main.listen(port, () => {
+//     console.log(`server is running at http://localhost:${port}`);
+// });
 
-// export const api = functions.https.onRequest({ region: 'europe-west1' }, app);
+export const api = functions.https.onRequest({ region: 'europe-west1' }, app);
